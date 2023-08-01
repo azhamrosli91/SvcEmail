cfcf73d137647941e6d68d0db2417e60e043a3de
azhamrosli91 <137070066+azhamrosli91@users.noreply.github.com>
2023-08-01 22:35:49 +0800

    Setting

--- Program.cs
+++ Program.cs
@@ -28,9 +28,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+   
 }
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseCors(builder => builder
                  .AllowAnyHeader()
                  .AllowAnyMethod()
